3c4e6a5d51c46b1d6d1b29b89d7a4eae262e42cf
Alexis <ortegafierrojorgealexis@gmail.com>
2024-11-12 18:43:02 -0600

    test21

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+    console.log('Si tiene Service Worker');
+
+    navigator.serviceWorker.register('/sw.js')  // Asegúrate de que la ruta sea correcta
+        .then(res => console.log('Service Worker cargando correctamente', res))
+        .catch(err => console.log('Service Worker no se pudo cargar', err));  // Cambiado 'res' por 'err'
+} else {
+    console.log('No se localiza Service Worker');
+}
+
 if ("serviceWorker" in navigator) {
     console.log("si tiene");
   
